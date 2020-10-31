2fffb204ee055a1aa0587eca9c7464e57e88626a
jiwon-ryu <jiwon.ryu@snu.ac.kr>
2020-10-31 21:09:48 +0900

    converts rgb to gray, resizes gray, and saves gray as jpg & csv

--- img_converter.py
+++ img_converter.py
@@ -1,9 +1,9 @@
-import numpy as np
 '''
     1. input_folder의 rgb 이미지 -> grayscale에 gray로 저장
-    2. grayscale -> resize -> jpg, txt로 저장
+    2. grayscale -> resize -> jpg, csv로 저장
 '''
 
+import numpy as np
 import csv
 import glob
 import cv2
@@ -14,57 +14,19 @@ input_folder = input_folder[0:len(input_folder)]
 output_folder = sorted(glob.glob('/users/jiwon/documents/강의/2020-2/기계시스템설계2/grayscale/*/'))
 
 def txtWriter(single_img, save_name):
-    save_name1 = save_name + '.txt'
-    save_name2 = save_name + '2.txt'
-    
     img = cv2.imread(single_img, cv2.IMREAD_GRAYSCALE)
     img = np.array(img)
     img2list = ''
-    n_digits = 0
-    stop_i = 0
-    stop_j = 0
-    breaker = False
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            if (i == 0) & (j == 0): # start: no ' '
+            if (i == 0) & (j == 0):
                 img2list += str(img[i, j])
-                n_digits = len(img2list)            
-            else: # others: with ' '
-                if n_digits < 32767:
-                    img2list += str(' ' + str(img[i, j]))
-                    n_digits = len(img2list)
-                    # if finished without overflowing -> save txt
-                    if (i == img.shape[0]-1) & (j == img.shape[1]-1):
-                        f = open(save_name1, 'w')
-                        f.write(img2list)
-                        f.close()
-                else: # if overflowed
-                    # save current data
-                    f1 = open(save_name1, 'w')
-                    f1.write(img2list)
-                    f1.close()
-                    # record stop point
-                    stop_i = i
-                    stop_j = j
+            else:
+                img2list += str(' ' + str(img[i, j]))
 
-                    # write the rest in a new file
-                    if img2list[-1] == ' ':
-                        img2list = ''
-                    else:
-                        img2list= ' '
-                    for m in np.arange(stop_i, img.shape[0]):
-                        for n in np.arange(stop_j, img.shape[1]):
-                            if (m == stop_i) & (n == stop_j):
-                                img2list += str(img[m, n])    
-                            else:
-                                img2list += str(' ' + str(img[m, n]))                                               
-                                f2 = open(save_name2, 'w')
-                                f2.write(img2list)
-                                f2.close()
-                    breaker = True
-                    break
-        if breaker == True:
-            break
+    f = open(save_name, 'w')
+    f.write(img2list)
+    f.close()
 
 def rgb2gray(img, save_name):
     img = Image.open(img).convert('L')
@@ -83,8 +45,8 @@ for path, save_path in zip(input_folder, output_folder):
        image = path + 'rgb/' + str(i*10+1) + '_rgb.jpg'
        save_name = save_path + str(i*10+1) + '_gray.jpg'
        rgb2gray(image, save_name)
-  
-# convert to gray -> resize gray jpg into 96x96 -> save as jpg & txt
+
+# convert to gray -> resize gray jpg into 96x96 -> save as jpg & csv
 input_folder = sorted(glob.glob('/users/jiwon/documents/강의/2020-2/기계시스템설계2/grayscale/*/'))
 for path in input_folder:
     for i in range(100):
@@ -92,10 +54,11 @@ for path in input_folder:
         image = path + str(i*10+1) + '_gray.jpg'
         save_name = path + 'resized_' + str(i*10+1) + '_gray.jpg'
         resize(image, save_name)
-        # txt
+        # csv
         image = save_name
-        save_name = path + 'resized_' + str(i*10+1) + '_gray'
-        txtWriter(image, save_name)          
+        txtWriter(image, save_name)
+
+
     
     
 
